cdfe93bfad26001fbbf28763b4e4b3ca81a28d49
Polina Skrobot <polinaskrobot@Polinas-iMac.local>
2021-11-02 17:46:58 -0700

    Changes in user cabinet to display prev orders

--- src/components/usercabinet/PreviousOrderItem.js
+++ src/components/usercabinet/PreviousOrderItem.js
@@ -24,16 +24,16 @@ export default function PreviousOrderItem(props) {
       <Box sx={{ display: "flex", flexDirection: "column" }}>
         <CardContent sx={{ flex: "1 0 auto" }}>
           <Typography component="div" variant="h6" color="primary">
-            On 12/10/2021 {props.name} was taking care of your child
+            On {props.date} {props.name} was taking care of your child
           </Typography>
           <Typography
             variant="subtitle1"
             color="text.secondary"
             component="div"
           >
-            Kate was awesome
+            {props.comment}
           </Typography>
-          <Rating name="read-only" value="3" size="small" readOnly />
+          <Rating name="read-only" value={props.rate} size="small" readOnly />
         </CardContent>
       </Box>
       <Stack> 
